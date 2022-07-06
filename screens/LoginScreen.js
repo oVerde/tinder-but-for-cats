c1adaab0c9cc655e938a7600f21d95d28ae63a19
Danilo Verde <danilo.verde@live.com>
2022-07-06 18:57:13 -0300

    :lipstick: feature: LoginScreen and content style

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,15 +1,37 @@
 import React from 'react';
 
-import { Text, View } from 'react-native';
+import { Text, StyleSheet, View } from 'react-native';
+import useAuth from '../hooks/useAuth';
 
 const LoginScreen = () => {
+  const { user } = useAuth();
   return (
-      <View>
-        <Text>
-          My Profile Screen
+      <View style={styles.loginScreen}>
+        <Text style={styles.loginHeader}>
+          { user ? (`02`) : (`Sign in`) })}
         </Text>
       </View>
+
   );
 };
+
+const styles = StyleSheet.create({
+
+  loginScreen: {
+    flex: 1,
+    flexDirection: 'column', //just to make sure it's a column
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#E5E5E5',
+  },
+
+  loginHeader: {
+    fontFamily: 'Nunito Sans',
+    fontSize: 126,
+    fontWeight: 'Bold',
+    letterSpacing: 0,
+    lineHeight: 32,
+  },
+});
 
 export default LoginScreen;
