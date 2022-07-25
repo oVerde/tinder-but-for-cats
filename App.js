3a8b2bd9142f7a271e89c115ef2b484665c84ce8
Danilo Verde <danilo.verde@live.com>
2022-07-25 14:03:42 -0300

    📦package: React Buddy setup and configuration.
    See more at: https://react-buddy.com/

--- App.js
+++ App.js
@@ -3,18 +3,22 @@ import TabNavigator from './TabNavigator';
 import { AuthProvider } from './hooks/useAuth';
 import { SafeAreaView } from 'react-native';
 import tw, { useDeviceContext } from 'twrnc';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { DevSupport } from '@react-buddy/ide-toolbox';
 
 
 export default function App() {
   useDeviceContext(tw);
   return (
-    <>
-        <StatusBar translucent/>
-        <SafeAreaView>
-          <AuthProvider>
-            <TabNavigator/>
-          </AuthProvider>
-        </SafeAreaView>
-    </>
+      <DevSupport>
+        <GestureHandlerRootView>
+          <StatusBar translucent/>
+          <SafeAreaView>
+            <AuthProvider>
+              <TabNavigator/>
+            </AuthProvider>
+          </SafeAreaView>
+        </GestureHandlerRootView>
+      </DevSupport>
   );
 }
